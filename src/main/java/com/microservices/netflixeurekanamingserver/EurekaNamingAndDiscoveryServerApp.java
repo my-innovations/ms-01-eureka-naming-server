d3649ae9acd91aa69d21ff9f63ecf6b482b52946
DELL <punyasmrutin@gmail.com>
2021-01-29 15:43:47 +0530

    Initial commit

---
+++ src/main/java/com/microservices/netflixeurekanamingserver/EurekaNamingAndDiscoveryServerApp.java
@@ -0,0 +1,13 @@
+package com.microservices.netflixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaNamingAndDiscoveryServerApp {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingAndDiscoveryServerApp.class, args);
+	}
+}
